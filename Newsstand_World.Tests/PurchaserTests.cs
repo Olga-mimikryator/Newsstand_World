7ea3cc78f9d550c0b557a652f7d0ddb273749823
ладно Омг <omg.ladno@bk.ru>
2026-06-23 10:49:48 +0300

    Пришлось переносить файлы .git и .github, чтобы цспешно закоммитить добавленные в ходе 4 практической работы файлы, поэтому список изменений в проекте очень страшный.
    Добавлены xUnit тесты для приложения и документ со сценариями тестирования

---
+++ Newsstand_World.Tests/PurchaserTests.cs
@@ -0,0 +1,32 @@
+﻿using Newsstand_World.Model;
+using System.ComponentModel.DataAnnotations;
+using Xunit;
+
+namespace Newsstand_World.Tests.Model
+{
+    public class PurchaserTests
+    {
+        [Fact]
+        public void Purchaser_WithValidData_ShouldBeValid()
+        {
+            // Arrange
+            var purchaser = new Purchaser
+            {
+                Name = "Иван",
+                LastName = "Иванов",
+                Phone = "+7-999-123-45-67",
+                BirthDate = new DateTime(1990, 5, 15)
+            };
+
+            var context = new ValidationContext(purchaser);
+            var results = new List<ValidationResult>();
+
+            // Act
+            var isValid = Validator.TryValidateObject(purchaser, context, results, true);
+
+            // Assert
+            Assert.True(isValid);
+            Assert.Empty(results);
+        }
+    }
+}
